e6639d50208664ecad9c7b43e1ce4e6c62938e2d
Birch <116130868+wyobirch@users.noreply.github.com>
2023-10-21 00:49:54 -0600

    Update

--- runRPi.py
+++ runRPi.py
@@ -6,15 +6,15 @@ import csv
 import os
 
 
-interval = 30 #delay between sensor readings in seconds
+interval = 5*60 #delay between sensor readings in seconds
 
 def run():
 	sensor1 = Sensor(15, 21)
 	sensor2 = Sensor(2, 27)	
-	
+
 	while (True):
 		seconds = floor(time())
-		if (seconds % 60 == 10):
+		if (seconds % 30*60 == 20): #send readings to database - needs to happen at a different time than when the reading is taken.
 			os.system("push.sh")
 		if (seconds % interval == 0):
 			reading = [seconds, sensor1.reading(), sensor2.reading()]
