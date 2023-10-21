71bfb82424902f8cb71bee26efb851fe244e02f5
Birch <116130868+wyobirch@users.noreply.github.com>
2023-10-21 15:19:12 -0600

    Update

--- format-index.py
+++ format-index.py
@@ -2,9 +2,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 import datetime
+import time
 import numpy as np
 
-def make_trend_png():
+def get_moisture_data():
     data = pd.read_csv('sample_data/data.csv').values
 
     X = data[max(-346,-len(data)):,0]
@@ -12,6 +13,7 @@ def make_trend_png():
     y = 1-np.average(data[max(-168,-len(data)):,1:], axis=1)
 
     fig = plt.figure(figsize=(10, 2))
+    fig.gca().yaxis.set(ticks=np.arange(-.5, 2, .5))
 
     plt.scatter(X,y, alpha=.5)
 
@@ -19,22 +21,30 @@ def make_trend_png():
     plt.plot(X, np.array([.25]*len(X)), '--r')
 
     current_values = plt.gca().get_yticks()
-    plt.gca().set_yticklabels(["", "too-dry", "ideal", "too-wet", ""])
+
+    plt.ylim((-0.25,1.25))
+    plt.gca().set_yticklabels("too-dry" if x == 0 else ("optimal" if x == .5 else ("too-wet" if x == 1 else "")) for x in current_values)
 
     plt.gcf().autofmt_xdate()
     
     plt.savefig('moisture-trend.png')
-    return 'moisture-trend.png'
+    current_moisture = ("low", "optimal", 'high')[int(2*y[-1])]
+    return 'moisture-trend.png', current_moisture
 
 def tag(tag, text="", options=""):
     return f"<{tag} {options}>"+text+f"</{tag}>"
 
-def get_suggested_action(): #TODO make this dynamic
-    return "watter your lawn"
+def get_suggested_action(current_moisture): #TODO make this dynamic
+    if (current_moisture == 'low'):
+        return "water the lawn"
+    elif (current_moisture == 'optimal'):
+        return "water if sunny"
+    elif (current_moisture == 'high'):
+        return "don't water the lawn"
 
 def make_html():
-    trend_png = make_trend_png()
-    action = get_suggested_action()
+    trend_png, current_moisture = get_moisture_data()
+    action = get_suggested_action(current_moisture)
 
     with open('index.html', 'w') as site:
         site.write(tag("h", "Lawn moisture monitor"))
@@ -42,7 +52,13 @@ def make_html():
         site.write(tag("h4", "last weeks trend:"))
         site.write(tag("img", options=f"src=\"{trend_png}\""))
         site.write(tag("br"))
+        site.write(tag("p", f"Current moisture content: {current_moisture}"))
         site.write(tag("p", f"Suggested action: {action}"))
 
-
+with open('sample_data/data.csv', 'a') as f:
+  f.write(f"{time.time()}, 0,0\n")
 make_html()
+
+with open('index.html', 'r') as f:
+  for line in f:
+    print(line)
