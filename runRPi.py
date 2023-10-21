914d5ed474ebbba33545a6c586c65ade4da46f00
Birch-Phillips <116130868+wyobirch@users.noreply.github.com>
2023-10-20 23:33:23 -0600

    Update

--- runRPi.py
+++ runRPi.py
@@ -1,5 +1,4 @@
-from sensor1 import Sensor
-import sensor2
+from sensor import Sensor
 from time import time, sleep
 from math import floor
 import csv
@@ -8,12 +7,12 @@ interval = 5
 
 def run():
 	sensor1 = Sensor(15, 21)
-	sensor2.setup()
+	sensor2 = Sensor(2, 27)
 	
 	while (True):
 		seconds = floor(time())
 		if (seconds % interval == 0):
-			reading = [seconds, sensor1.reading(), sensor2.getReading()]
+			reading = [seconds, sensor1.reading(), sensor2.reading()]
 			print(reading)
 			with open('data.csv', 'a') as log:
 				writer = csv.writer(log, dialect='excel')
